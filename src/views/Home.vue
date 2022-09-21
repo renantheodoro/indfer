b5c04edc21e6890b50628b5de5ac8a203fac4269
renan.theodoro <renan.theodoro@simlabsdev.onmicrosoft.com>
2022-09-20 23:02:04 -0300

    feat: start home

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="blue">Indfer</h1>
+        <h2 class="orange">Indfer</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'app-home',
+
+}
+</script>
+<style lang="">
+    
+</style>
